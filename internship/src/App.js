4a6a161aa31938ad88b2ebd29b635d0f37030d35
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-04-17 02:11:26 +0530

    UPDATE: App

--- internship/src/App.js
+++ internship/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import LandingPage from './Screens/LandingPage/LandingPage';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <LandingPage />
     </div>
   );
 }
+
 
 export default App;

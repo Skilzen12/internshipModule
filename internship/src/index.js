cc2281ec242b88303ff4290bfb2ee2c1ada375f5
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-04-20 10:19:12 +0530

    UPDATE: App

--- internship/src/index.js
+++ internship/src/index.js
@@ -6,40 +6,34 @@ import LandingPage from './Screens/LandingPage/LandingPage';
 import JobGrid from './Screens/JobGrid/Job_Grid';
 import Login from './Screens/Auth/Login/Login';
 import SignUp from './Screens/Auth/SignUp/SignUp';
-import {MultiStepForm} from './Screens/Auth/MultiStepForm';
-import {OrganizationMultiStep} from './Screens/Auth/OrganizationMultiStep';
 import PostInternship from './Screens/PostInternship/Post_intern';
 import CandidateProfile from './Screens/CandidateProfile/Candidate';
 import CompanyProfile from './Screens/CompanyProfile/Company_profile';
 import InternshipProfile from './Screens/InternshipProfile/Comp_profile2'; 
 import CompanyDashboard from './Screens/Dashboard/DashboardMain'; 
-import { UserProvider, StudentProvider, CompanyProvider } from './utility/contexts/userContext';
+import CompanySpam from './Screens/AdminDashboard/CompanySpam';
+import AdminDashboardMain from './Screens/AdminDashboard/AdminDashboardMain';
+
 import './index.css';
 
 ReactDOM.render(
   <>
-    <UserProvider>
-      <StudentProvider>
-        <CompanyProvider>
-          <Router>
-            <Switch>
-              <Route exact path='/' component={App} />
-              <Route exact path='/home' component={LandingPage} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/signup' component={SignUp} />
-              {/* <Route exact path='/userForm' component={MultiStepForm} />
-              <Route exact path='/orgForm' component={OrganizationMultiStep} /> */}
-              <Route exact path='/postInternship' component={PostInternship} />
-              <Route exact path='/jobGrid' component={JobGrid} />
-              <Route exact path='/candidate' component={CandidateProfile} />
-              <Route exact path='/company' component={CompanyProfile} />
-              <Route exact path='/internship' component={InternshipProfile} />
-              <Route exact path='/dashboard' component={CompanyDashboard} />
-            </Switch>             
-          </Router>
-        </CompanyProvider>
-      </StudentProvider>
-    </UserProvider>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route exact path='/home' component={LandingPage} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={SignUp} />
+        <Route exact path='/postInternship' component={PostInternship} />
+        <Route exact path='/jobGrid' component={JobGrid} />
+        <Route exact path='/candidate' component={CandidateProfile} />
+        <Route exact path='/company' component={CompanyProfile} />
+        <Route exact path='/internship' component={InternshipProfile} />
+        <Route exact path='/dashboard' component={CompanyDashboard} />
+        <Route exact path='/adminDashboard' component={AdminDashboardMain} />
+        <Route exact path='/companyspam' component={CompanySpam} />
+      </Switch>             
+    </Router>
   </>,
   document.getElementById('root')
 );

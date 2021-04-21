25034cf559acabeef8ecd98c728de76b2391035b
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-04-21 06:18:42 +0530

    Internship Profile

--- internship/src/Screens/InternshipProfile/Comp_profile2.js
+++ internship/src/Screens/InternshipProfile/Comp_profile2.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from 'react';
 import './Comp_profile2.css';
@@ -5,16 +6,146 @@ import  {dataCmp2} from '../../utility/DummyData/InternshipProfile';
 import Back from "../../Components/GoBack/Back_comp";
 import Header from "../../Components/Header/Updated_Header";
 import Footer from '../../Components/Footer/Footer';
-import {BsBookmark as Mark} from "react-icons/bs";
-import {TagsIcons} from '../../Components/LandingPage/FeaturedCards/FeaturedCards'
+import {BsBookmark as Mark, BsFillBookmarkFill} from "react-icons/bs";
 import AdminService from '../../AdminServices/AdminService';
 import {LogoMap} from '../../utility/Maps/LandingPageMaps';
+import { HiUserGroup } from 'react-icons/hi';
+
+const InternshipProfile = ({obj, id}) => {
+    function getDate(date){
+        var postedDate = date.split('T')[0];
+        var PostedDate = postedDate.split('-')[2];
+        var PostedMonth = postedDate.split('-')[1];
+        var PostedYear = postedDate.split('-')[0];
+
+        return `${PostedDate}/${PostedMonth}/${PostedYear}`;
+    }
+    return(
+        <div className="box1">
+            <div className='adjusting_card'>
+                <div className="company_card">
+                    <div className="for_margin_inside">
+                        <div className="for_header_side">
+                            <div>
+                                <>
+                                    <p className="for_post_title">{obj.title ? obj.title : 'ABCD EFG'}</p>
+                                    <p className='company_cat_type2' style={{marginTop: 0}} >{obj.company ? obj.company.kind : 'B2A' }</p>
+                                </>
+                            </div>
+                            <div className='img_n_name2'>
+                                {obj.company ? (
+                                    <img src={LogoMap.get(obj.company.name).url} className="companyLogo__featuredCards" alt="" />
+                                ) : <HiUserGroup style={{fontSize: 40}} /> }
+                                <div style={{marginTop:'5px'}}>
+                                    <p className='company_main_name'>{obj.company ? obj.company.name : 'XASD'}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="btn_row">
+                            <div className="adj_btn_comp">
+                                <button className="btn_for_apply btn_for_card" disabled={obj.is_applied} onClick={() => Apply(id)}>{obj.is_applied ? 'Applied' : 'Apply Now'}</button>
+                            </div>
+                            <div className="adj_btn_comp">
+                                <button className="btn_for_apply btn_for_card" disabled={obj.is_marked} onClick={() => Bookmark(id)}>
+                                    {obj.is_marked ? <BsFillBookmarkFill style={{fontSize:17,paddingBottom:'2px', marginRight: 5}}/> : <Mark style={{fontSize:17, marginRight: 5, paddingBottom:'2px'}}/>}
+                                    {obj.is_marked ? 'Saved' : 'Bookmark it'}
+                                </button>
+                            </div>
+                        </div>
+                        
+                        <hr></hr>
+                        <div className="short_details">
+                            <div className="each_short_detail2">
+                                <p className="specification2">Stipend</p>
+                                <p className="specification_ans2">{obj.min_stipend ? `${obj.min_stipend} - ${obj.max_stipend} INR` : '20K-50K'}</p>
+                            </div>
+                            <div className="each_short_detail2">
+                                <p className="specification2">Internship type</p>
+                                <p className="specification_ans2">{obj.kind ? obj.kind : 'Full Time'}</p>                                                        
+                            </div>
+                            <div className="each_short_detail2">
+                                <p className="specification2">Location</p>
+                                <p className="specification_ans2">{obj.company_location ? obj.company_location.location : 'Aryacet' }</p>
+                            </div>
+                            <div className="each_short_detail2">
+                                <p className="specification2">Internship duration</p>
+                                <p className="specification_ans2">{obj.duration ? obj.duration : '5 months' }</p>
+                            </div>
+                            <div className="each_short_detail2">
+                                <p className="specification2">Internship start date</p>
+                                <p className="specification_ans2">{obj.start_date ? getDate(obj.start_date) : '3/05/2021'}</p>
+                            </div>
+                            <div className="each_short_detail2">
+                                <p className="specification2">Number of openings</p>
+                                <p className="specification_ans2">{obj.openings ? obj.openings : 5}</p>
+                            </div>
+                            
+                            <div className="each_short_detail2">
+                                <p className="specification2">Perks</p>
+                                {
+                                    obj.perks ? <TagsIcons list={obj.perks} /> :
+                                    <TagsIcons list={['Certificate','5 days a week','Letter of recommendation']} />
+                                }
+                            </div>
+                            <div className="each_short_detail2">
+                                <p className="specification2">Skills required</p>
+                                {
+                                    obj.skills ? <TagsIcons list={obj.skills} /> : <TagsIcons list={['Editing','Wire-framing','XD','User Person','Sketch']} />
+                                }
+                            </div>
+                            <div className="each_short_detail2" style={{paddingLeft: 10}}>
+                                <p className="specification2">Pre-Placement offer</p>
+                                <p className="specification_ans2">{obj.ppo ? obj.ppo : 'No'}</p>
+                            </div>
+                        </div>
+                        <hr  className="for_hr_2"></hr>
+                        <div>
+                            <h3 className="company_heading2">Job Description</h3>
+                            <p className="content_area2">{obj.description}</p>
+                            <hr className="hr_for_TM"></hr>
+                            <div className="adj_btn_comp">
+                                <button className="btn_for_apply btn_for_card" disabled={obj.is_applied} onClick={() => Apply(id)}>{obj.is_applied ? 'Applied' : 'Apply Now'}</button>
+                            </div>
+                        </div>
+                    </div>                                            
+                </div>        
+            </div>
+        </div>
+    );
+}
+
+const TagsIcons =({list})=>{ 
+    return (
+      <ul className="tags__featured" style={{paddingLeft: 0, paddingRight: 0}}>
+        {list.map(item=>(
+          <li className="mt-1">
+            <a className="bg-regent-opacity-15 min-width-px-96 text-center rounded-3 py-1"
+              style={{ fontFamily: "Gordita" }}
+            >
+              {item.name ? item.name : item}
+            </a>
+          </li>
+        ))}
+      </ul>
+    )
+}
+
+const Apply = async (id) => {
+    AdminService.InternshipsApply(id)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
+const Bookmark = async (id) => {
+    AdminService.InternshipsApply(id)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
 
 function AboutCmp (){
     let [data, setData] = useState([]);
     var id = '';
     
-    const getDetails = (id) => {
+    const getDetails = async (id) => {
         AdminService.getInternshipsDetail(id)
             .then(res => setData(res.data))
             .catch(err => console.log(err))
@@ -27,9 +158,7 @@ function AboutCmp (){
     window.onload = () => {         
         id = getIDFromURL();
         getDetails(id); 
-    }      
-    
-    console.log(id, data);
+    }    
 
     return(
         <>
@@ -40,124 +169,7 @@ function AboutCmp (){
                             <div className='adjusting_card_back'>
                                 <Back/>
                             </div>
-                                <div className="box1">
-                                    
-                                        <div className='adjusting_card'>
-                                            <div className="company_card">
-                                                <div className="for_margin_inside">
-                                                    <div className="for_header_side">
-                                                        <div>
-                                                            {/* {data !== [] ? (
-                                                                <>
-                                                                    <p className="for_post_title">{data.title}</p>
-                                                                    <p className='company_cat_type2' >{data.company.kind}</p>
-                                                                </>
-                                                            ) : <>  */}
-                                                                    <p className="for_post_title">{dataCmp2.title}</p>
-                                                                    <p className='company_cat_type2' >{dataCmp2.category_type}</p>
-                                                                {/* </>
-                                                            } */}
-                                                        </div>
-                                                        <div className='img_n_name2'>
-                                                            <div className='cmp_main_img'>
-                                                                {/* {data.company !== "" ? (
-                                                                    <img src={LogoMap.get(data.company.name).url} className="main_img" />
-                                                                ) :  */}
-                                                                    <img src={dataCmp2.cmp_img} className="main_img" />
-                                                                {/* }                                                                 */}
-                                                            </div>
-                                                            <div style={{marginTop:'5px'}}>
-                                                                {/* {data.company.name ? (
-                                                                    <p className='company_main_name'>{data.company.name}</p>
-                                                                ) :  */}
-                                                                <p className='company_main_name'>{dataCmp2.company_name}</p>
-                                                                {/* }                                                                  */}
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div className="btn_row">
-                                                        <div className="adj_btn_comp">
-                                                            <button className="btn_for_apply btn_for_card">Apply Now</button>
-                                                        </div>
-                                                        <div className="adj_btn_comp">
-                                                            <button className="btn_for_apply btn_for_card"><Mark style={{fontSize:17,paddingBottom:'2px'}}/>&nbsp;&nbsp;Save it</button>
-                                                        </div>
-                                                    </div>
-                                                    
-                                                    <hr></hr>
-                                                    <div className="short_details">
-                                                        <div className="each_short_detail2">
-                                                            <p className="specification2">Stipend</p>
-                                                            <p className="specification_ans2">{dataCmp2.Stipend}</p>
-                                                        </div>
-                                                        <div className="each_short_detail2">
-                                                            <p className="specification2">Internship type</p>
-                                                            <p className="specification_ans2">{dataCmp2.inter_type}</p>                                                        </div>
-                                                        <div className="each_short_detail2">
-                                                            <p className="specification2">Location</p>
-                                                            <p className="specification_ans2">{dataCmp2.location}</p>
-                                                        </div>
-                                                        <div className="each_short_detail2">
-                                                            <p className="specification2">Internship duration</p>
-                                                            <p className="specification_ans2">{dataCmp2.intern_duration}</p>
-                                                        </div>
-                                                        <div className="each_short_detail2">
-                                                            <p className="specification2">Internship start date</p>
-                                                            <p className="specification_ans2">{dataCmp2.inter_start}</p>
-                                                        </div>
-                                                        <div className="each_short_detail2">
-                                                            <p className="specification2">Number of openings</p>
-                                                            <p className="specification_ans2">{dataCmp2.no_of_opens}</p>
-                                                        </div>
-                                                        
-                                                        <div className="each_short_detail2">
-                                                            <p className="specification2">Perks</p>
-                                                            {
-                                                                <TagsIcons list={['Certificate','5 days a week','Letter of recommendation']} />
-                                                            }
-                                                            {/* <p className="specification_ans2">{dataCmp2.perks}</p> */}
-                                                        </div>
-                                                        <div className="each_short_detail2">
-                                                            <p className="specification2">Skills required</p>
-                                                            {
-                                                                <TagsIcons list={['Editing','Wire-framing','XD','User Person','Sketch']} />
-                                                            }
-                                                            {/* <p className="specification_ans2">{dataCmp2.est_Since}</p> */}
-                                                        </div>
-                                                        <div className="each_short_detail2">
-                                                            <p className="specification2">Pre-Placement offer</p>
-                                                            <p className="specification_ans2">{dataCmp2.ppo}</p>
-                                                        </div>
-                                                        {/* <div className="each_short_detail2">
-                                                            <p className="specification2">Soft skills</p>
-                                                            <ul className="list_of_ss">
-                                                                <li className="list_gap2">Slack</li>
-                                                                <li className="list_gap2">Basic English</li>
-                                                                <li className="list_gap2">Well Organized</li>
-                                                            </ul>                                                        
-                                                        </div> */}
-                                                        
-                                                        
-                                                    </div>
-                                                    <hr  className="for_hr_2"></hr>
-                                                    <div>
-                                                        <h3 className="company_heading2">{dataCmp2.heading}</h3>
-                                                        <p className="content_area2">{dataCmp2.text}</p>
-                                                        <hr className="hr_for_TM"></hr>
-                                                        <div className="adj_btn_comp">
-                                                            <button className="btn_for_apply btn_for_card">Apply Now</button>
-                                                        </div>
-                                                    </div>
-                                                    {/* <p className="team_members_heading">Team Members:</p>
-                                                    <div>
-                                                        <Team/>
-                                                    </div> */}
-                                                </div>
-                                                {/* <hr></hr> */}                                                
-                                            </div>        
-                                        </div>
-                                    
-                                </div>
+                            {data ? <InternshipProfile obj={data} id={id} /> : null }          
                         </div>
                         <Footer></Footer>
                     </div>
